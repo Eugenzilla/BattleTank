fde0031104c681bc41276e6774c357c740f1110d
EugenzillaTests <r3gzzz@gmail.com>
2021-07-24 19:15:02 +0200

    Graphics test with moving circles

--- src/io/eugenzilla/display/Display.java
+++ src/io/eugenzilla/display/Display.java
@@ -2,6 +2,10 @@ package io.eugenzilla.display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
 public abstract class Display {
 
@@ -9,7 +13,16 @@ public abstract class Display {
     private static JFrame window;
     private static Canvas content;
 
-    public static void create(int width, int height, String title) {
+    public static BufferedImage bufferedImage;
+    private static int[] bufferedImageData;
+    private static Graphics bufferedImageGraphics;
+    private static int clearColor; // цвет для очищения картинки
+
+    private static BufferStrategy bufferStrategy;
+
+    private static float delta = 0;
+
+    public static void create(int width, int height, String title, int _clearColor, int numBuffers) {
         if (isCreated) {
             return;
         }
@@ -28,7 +41,40 @@ public abstract class Display {
         window.setLocationRelativeTo(null); // окно по центру
         window.setVisible(true);
 
+        bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        bufferedImageData = ((DataBufferInt) bufferedImage.getRaster().getDataBuffer()).getData(); // вытащить массив интегеров из изображения
+        bufferedImageGraphics = bufferedImage.getGraphics();
 
+        clearColor = _clearColor;
+
+        content.createBufferStrategy(numBuffers);
+        bufferStrategy = content.getBufferStrategy();
+
+        isCreated = true;
+    }
+
+    public static void clear() {
+        Arrays.fill(bufferedImageData, clearColor);
+    }
+
+    public static void render() {
+        bufferedImageGraphics.setColor(new Color(0xff0000ff)); //синий
+        bufferedImageGraphics.fillOval((int)(350 + Math.sin(delta)*200), 150, 100, 100);
+
+        ((Graphics2D)bufferedImageGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        bufferedImageGraphics.fillOval((int)(350 + Math.sin(delta)*-200), 350, 100, 100);
+
+        ((Graphics2D)bufferedImageGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+
+
+        delta += 0.02f;
+    }
+
+    public static void swapBuffers() {
+        Graphics graphics = bufferStrategy.getDrawGraphics(); // вытаскиваем текущую графику канваса
+        graphics.drawImage(bufferedImage, 0, 0, null); // заменяем графику на буфер
+        bufferStrategy.show();
     }
 
 }
--- src/io/eugenzilla/main/Main.java
+++ src/io/eugenzilla/main/Main.java
@@ -2,11 +2,26 @@ package io.eugenzilla.main;
 
 import io.eugenzilla.display.Display;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Display.create(800, 600, "Battle Tank");
+        Display.create(800, 600, "Battle Tank", 0xff00ff00, 3); // argb цвет непрозрачный зеленый
 
+        Timer timer = new Timer(1000 / 60, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) { // функция будет вызываться каждый интервал времени
+                Display.clear();
+                Display.render();
+                Display.swapBuffers();
+
+            }
+        });
+
+        timer.setRepeats(true); // повторять Action
+        timer.start();
     }
 }
